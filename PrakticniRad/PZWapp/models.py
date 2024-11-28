30340e039c4dcd4cf445032923f9c27f85ac7fe1
Larax3 <164391276+Larax3@users.noreply.github.com>
2024-11-28 10:24:59 +0100

    dodavanje modela

--- PrakticniRad/PZWapp/models.py
+++ PrakticniRad/PZWapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class VrtnaBiljka(models.Model):
+    ime_v = models.CharField(max_length = 30)
+    slikaBiljke_v = models.ImageField()
+    regijaBiljke_v = models.CharField(max_length = 20)
+    vrijemeSazrijevanja_v = models.CharField(max_length = 15)
+    
+
+
+class PovrtnaBiljka(models.Model):
+    ime_p = models.CharField(max_length = 30)
+    slikaBiljke_p = models.ImageField()
+    regijaBiljke_p = models.CharField(max_length = 20)
+    vrijemeSazrijevanja_p = models.CharField(max_length = 15)
+
