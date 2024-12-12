01a67052186ca3810898ef5388ab0103dd8332df
Larax3 <164391276+Larax3@users.noreply.github.com>
2024-12-12 15:03:13 +0100

    sljedeci commit

--- PrakticniRad/PrakticniRad/urls.py
+++ PrakticniRad/PrakticniRad/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from . import views
+from PZWapp import views
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
